0caa11db13bbe6ac013834cf9e6f5cd59c3699fc
Max M <maxmorhardt13@gmail.com>
2023-05-08 19:56:14 -0400

    added firefox support over chrome

--- src/main/java/co/lockpass/tvapi/selenium/SeleniumUtil.java
+++ src/main/java/co/lockpass/tvapi/selenium/SeleniumUtil.java
@@ -3,8 +3,8 @@ package co.lockpass.tvapi.selenium;
 import co.lockpass.tvapi.loggerwrapper.Logger;
 import co.lockpass.tvapi.loggerwrapper.LoggerFactory;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
@@ -14,7 +14,7 @@ import java.time.Duration;
 public class SeleniumUtil {
 
     private final Logger logger = LoggerFactory.getLogger(SeleniumUtil.class);
-    private final String DRIVER_PATH = System.getProperty("user.home") + File.separator + "chromedriver" + File.separator + "chromedriver";
+    private final String DRIVER_PATH = System.getProperty("user.home") + File.separator + "drivers" + File.separator + "geckodriver";
     private final int TIMEOUT_SEC = 30;
 
     private WebDriver driver;
@@ -23,21 +23,17 @@ public class SeleniumUtil {
         logger.start("======== initializing selenium ========");
 
         logger.info("setting driver path to: " + DRIVER_PATH);
-        System.setProperty("webdriver.chrome.driver", DRIVER_PATH);
+        System.setProperty("webdriver.gecko.driver", DRIVER_PATH);
 
-        logger.info("creating chrome options");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+        logger.info("creating options");
+        FirefoxOptions options = new FirefoxOptions();
 
         logger.info("creating driver");
-        driver = new ChromeDriver(options);
+        driver = new FirefoxDriver(options);
 
         logger.info("creating timeout settings");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TIMEOUT_SEC));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(TIMEOUT_SEC));
-
-        logger.info("making window fullscreen");
-        driver.manage().window().fullscreen();
 
         logger.success("======== completed selenium initialization ========");
     }
